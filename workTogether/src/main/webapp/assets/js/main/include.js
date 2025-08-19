f644d5384ea3be80a9eb33b7a7d6e6cc240d0d30
BuddleK <kangbudle01@gmail.com>
2025-08-19 12:19:43 +0900

    250819 12:19 강버들 내용 저장

--- workTogether/src/main/webapp/assets/js/main/include.js
+++ workTogether/src/main/webapp/assets/js/main/include.js
@@ -3,21 +3,21 @@ window.addEventListener("DOMContentLoaded", () => {
 
   if (isLogin) {
     // 헤더 불러오기(로그인)
-    fetch("./../../header_login.html")
+    fetch("./../../header_login.jsp")
       .then((response) => response.text())
       .then((data) => {
         document.getElementById("header").innerHTML = data;
       });
   } else {
     // 헤더 불러오기(비로그인)
-    fetch("./../../header.html") //임시
+    fetch("./../../header.jsp") //임시
       .then((response) => response.text())
       .then((data) => {
         document.getElementById("header").innerHTML = data;
       });
   }
   // 푸터 불러오기
-  fetch("./../../footer.html")
+  fetch("./../../footer.jsp")
     .then((response) => response.text())
     .then((data) => {
       document.getElementById("footer").innerHTML = data;
