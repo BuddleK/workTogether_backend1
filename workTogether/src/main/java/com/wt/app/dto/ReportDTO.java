6cb1a09e11a6cb6f33c42074659d2cf49feca9cf
jeabin5792 <woqls0512@naver.com>
2025-08-20 18:55:36 +0900

    250820 18:55 이재빈

--- workTogether/src/main/java/com/wt/app/dto/ReportDTO.java
+++ workTogether/src/main/java/com/wt/app/dto/ReportDTO.java
@@ -1,18 +1,7 @@
 package com.wt.app.dto;
 
 public class ReportDTO {
-//	-- 게시글
-//	CREATE TABLE tbl_posts (
-//	    posts_number NUMBER PRIMARY KEY,
-//	    posts_title VARCHAR2(300) NOT NULL,
-//	    users_number NUMBER NOT NULL,
-//	    posts_content VARCHAR2(4000) NOT NULL,
-//	    posts_view_count NUMBER DEFAULT 0 NOT NULL,
-//	    posts_created_date DATE NOT NULL,
-//	    posts_updated_date DATE,
-//	    posts_report_count NUMBER DEFAULT 0 NOT NULL,
-//	    CONSTRAINT fk_posts_users FOREIGN KEY (users_number) REFERENCES tbl_users(users_number) ON DELETE CASCADE
-//	);
+
 	
 	
 }
