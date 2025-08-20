70d41db4590f2690afab37e1a517f8f3266daa19
dece1225 <dece1225@naver.com>
2025-08-20 20:33:35 +0900

    250820 20:33 김태현 페이지 DTO 완성

--- workTogether/src/main/java/com/wt/app/dto/NormalReferenceListDTO.java
+++ workTogether/src/main/java/com/wt/app/dto/NormalReferenceListDTO.java
@@ -1,5 +1,67 @@
 package com.wt.app.dto;
 
 public class NormalReferenceListDTO {
-	
+	int referenceNumber;
+	int usersNumber;
+
+	String usersName;
+	String referenceTitle;
+	String referenceCreatedDate;
+	boolean answeredYn;
+
+	public int getReferenceNumber() {
+		return referenceNumber;
+	}
+
+	public void setReferenceNumber(int referenceNumber) {
+		this.referenceNumber = referenceNumber;
+	}
+
+	public int getUsersNumber() {
+		return usersNumber;
+	}
+
+	public void setUsersNumber(int usersNumber) {
+		this.usersNumber = usersNumber;
+	}
+
+	public String getUsersName() {
+		return usersName;
+	}
+
+	public void setUsersName(String usersName) {
+		this.usersName = usersName;
+	}
+
+	public String getReferenceTitle() {
+		return referenceTitle;
+	}
+
+	public void setReferenceTitle(String referenceTitle) {
+		this.referenceTitle = referenceTitle;
+	}
+
+	public String getReferenceCreatedDate() {
+		return referenceCreatedDate;
+	}
+
+	public void setReferenceCreatedDate(String referenceCreatedDate) {
+		this.referenceCreatedDate = referenceCreatedDate;
+	}
+
+	public boolean isAnsweredYn() {
+		return answeredYn;
+	}
+
+	public void setAnsweredYn(boolean answeredYn) {
+		this.answeredYn = answeredYn;
+	}
+
+	@Override
+	public String toString() {
+		return "NormalReferenceListDTO [referenceNumber=" + referenceNumber + ", usersNumber=" + usersNumber
+				+ ", usersName=" + usersName + ", referenceTitle=" + referenceTitle + ", referenceCreatedDate="
+				+ referenceCreatedDate + ", answeredYn=" + answeredYn + "]";
+	}
+
 }
