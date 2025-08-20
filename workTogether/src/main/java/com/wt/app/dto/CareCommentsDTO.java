3118c8e917d7d55d703307615515776efbbbd646
HwangSeungU <pett1475321@gmail.com>
2025-08-21 01:58:24 +0900

    250821 0158 황승우 매퍼;

--- workTogether/src/main/java/com/wt/app/dto/CareCommentsDTO.java
+++ workTogether/src/main/java/com/wt/app/dto/CareCommentsDTO.java
@@ -1,6 +1,7 @@
 package com.wt.app.dto;
 
 public class CareCommentsDTO {
+	
 	private int commentsNumber;
 	private int normalNumber;
 	private int careNumber;
@@ -8,50 +9,84 @@ public class CareCommentsDTO {
 	private String commentsCreatedDate;
 	private String commentsUpdatedDate;
 	private String usersName;
+	
+	
 	public int getCommentsNumber() {
 		return commentsNumber;
 	}
+
+
 	public void setCommentsNumber(int commentsNumber) {
 		this.commentsNumber = commentsNumber;
 	}
+
+
 	public int getNormalNumber() {
 		return normalNumber;
 	}
+
+
 	public void setNormalNumber(int normalNumber) {
 		this.normalNumber = normalNumber;
 	}
+
+
 	public int getCareNumber() {
 		return careNumber;
 	}
+
+
 	public void setCareNumber(int careNumber) {
 		this.careNumber = careNumber;
 	}
+
+
 	public String getCommentsContent() {
 		return commentsContent;
 	}
+
+
 	public void setCommentsContent(String commentsContent) {
 		this.commentsContent = commentsContent;
 	}
+
+
 	public String getCommentsCreatedDate() {
 		return commentsCreatedDate;
 	}
+
+
 	public void setCommentsCreatedDate(String commentsCreatedDate) {
 		this.commentsCreatedDate = commentsCreatedDate;
 	}
+
+
 	public String getCommentsUpdatedDate() {
 		return commentsUpdatedDate;
 	}
+
+
 	public void setCommentsUpdatedDate(String commentsUpdatedDate) {
 		this.commentsUpdatedDate = commentsUpdatedDate;
 	}
+
+
+	public String getUsersName() {
+		return usersName;
+	}
+
+
+	public void setUsersName(String usersName) {
+		this.usersName = usersName;
+	}
+
+
 	@Override
 	public String toString() {
-		return "CommentsDTO [commentsNumber=" + commentsNumber + ", normalNumber=" + normalNumber + ", careNumber="
+		return "CareCommentsDTO [commentsNumber=" + commentsNumber + ", normalNumber=" + normalNumber + ", careNumber="
 				+ careNumber + ", commentsContent=" + commentsContent + ", commentsCreatedDate=" + commentsCreatedDate
-				+ ", commentsUpdatedDate=" + commentsUpdatedDate + "]";
+				+ ", commentsUpdatedDate=" + commentsUpdatedDate + ", usersName=" + usersName + "]";
 	}
-	
-	
 	
 	
 }
