7ad356ba9ba571c6802e8c521d8a835f623c22c6
dece1225 <dece1225@naver.com>
2025-08-20 19:44:22 +0900

    250820 19:44 김태현 DTO 제작 중

---
+++ workTogether/src/main/java/com/wt/app/dto/NormalPostsListDTO.java
@@ -0,0 +1,47 @@
+package com.wt.app.dto;
+
+public class NormalPostsListDTO {
+	int postsNumber;
+	int userNumber;
+	String postsTitle;
+	String postsCreatedDate;
+
+	public int getPostsNumber() {
+		return postsNumber;
+	}
+
+	public void setPostsNumber(int postsNumber) {
+		this.postsNumber = postsNumber;
+	}
+
+	public int getUserNumber() {
+		return userNumber;
+	}
+
+	public void setUserNumber(int userNumber) {
+		this.userNumber = userNumber;
+	}
+
+	public String getPostsTitle() {
+		return postsTitle;
+	}
+
+	public void setPostsTitle(String postsTitle) {
+		this.postsTitle = postsTitle;
+	}
+
+	public String getPostsCreatedDate() {
+		return postsCreatedDate;
+	}
+
+	public void setPostsCreatedDate(String postsCreatedDate) {
+		this.postsCreatedDate = postsCreatedDate;
+	}
+
+	@Override
+	public String toString() {
+		return "NormalPostsListDTO [postsNumber=" + postsNumber + ", userNumber=" + userNumber + ", postsTitle="
+				+ postsTitle + ", postsCreatedDate=" + postsCreatedDate + "]";
+	}
+
+}
