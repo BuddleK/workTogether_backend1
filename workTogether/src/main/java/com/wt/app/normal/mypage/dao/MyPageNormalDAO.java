e1b344824e749702100673f3e1c0c07013b0b76e
dece1225 <dece1225@naver.com>
2025-08-22 10:36:10 +0900

    250822 10:

--- workTogether/src/main/java/com/wt/app/normal/mypage/dao/MyPageNormalDAO.java
+++ workTogether/src/main/java/com/wt/app/normal/mypage/dao/MyPageNormalDAO.java
@@ -1,7 +1,10 @@
 package com.wt.app.normal.mypage.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 
+import com.wt.app.dto.NormalModifyDTO;
 import com.wt.config.MyBatisConfig;
 
 public class MyPageNormalDAO {
@@ -10,11 +13,16 @@ public class MyPageNormalDAO {
 	public MyPageNormalDAO() {
 		sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
 	}
-	
-	public boolean checkPw(String usersPassword){
+
+	public boolean checkPw(String usersPassword) {
 		return (Integer) sqlSession.selectOne("myPageNormal.checkPw", usersPassword) < 1;
 	}
-	
-	
-	
+
+	public List<NormalModifyDTO> normalSearch() {
+		System.out.println("모든 게시글 조회하기 - normalSearch 메소드 실행");
+		List<NormalModifyDTO> list = sqlSession.selectList(null);
+		System.out.println("조회결과 : " + list);
+		return list;
+	}
+
 }
