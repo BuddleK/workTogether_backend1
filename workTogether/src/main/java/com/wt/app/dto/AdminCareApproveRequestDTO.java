0265c615e7027bf007f581d54bc3c631df4a5b74
jsj030513 <sungju03051323@gmail.com>
2025-08-23 17:43:49 +0900

    250823 17:43 장성주 관리자 돌봄 쪽 진행중

---
+++ workTogether/src/main/java/com/wt/app/dto/AdminCareApproveRequestDTO.java
@@ -0,0 +1,19 @@
+package com.wt.app.dto;
+
+public class AdminCareApproveRequestDTO {
+	private int usersNumber;
+
+	public int getUsersNumber() {
+		return usersNumber;
+	}
+
+	public void setUsersNumber(int usersNumber) {
+		this.usersNumber = usersNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "AdminCareApproveRequestDTO [usersNumber=" + usersNumber + "]";
+	}
+	
+}
---
+++ workTogether/src/main/java/com/wt/app/dto/AdminCareRejectRequestDTO.java
@@ -0,0 +1,24 @@
+package com.wt.app.dto;
+
+public class AdminCareRejectRequestDTO {
+	private int usersNumber;
+    private String rejectReason;
+	public int getUsersNumber() {
+		return usersNumber;
+	}
+	public void setUsersNumber(int usersNumber) {
+		this.usersNumber = usersNumber;
+	}
+	public String getRejectReason() {
+		return rejectReason;
+	}
+	public void setRejectReason(String rejectReason) {
+		this.rejectReason = rejectReason;
+	}
+	@Override
+	public String toString() {
+		return "AdminCareRejectRequestDTO [usersNumber=" + usersNumber + ", rejectReason=" + rejectReason + "]";
+	}
+
+    
+}
