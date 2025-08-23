d26f973ac95fba378adf34d276aecccd308a60db
dece1225 <dece1225@naver.com>
2025-08-23 14:11:28 +0900

    250823 14:11 김태현 관리자 mapper작업중

--- workTogether/src/main/java/com/wt/app/dto/AdminNewsBannerDTO.java
+++ workTogether/src/main/java/com/wt/app/dto/AdminNewsBannerDTO.java
@@ -1,10 +1,21 @@
 package com.wt.app.dto;
 
 public class AdminNewsBannerDTO {
-	//뉴스 테이블
+	// 뉴스 테이블
 	private int newsNumber; // 뉴스 번호
 	private String newsTitle; // 뉴스 제목
 	private String newsCreatedDate; // 뉴스 생성일
+
+	// 뉴스 베너 파일 테이블
+	private int banner_files_number; // 베너 파일 번호
+
+	public int getBanner_files_number() {
+		return banner_files_number;
+	}
+
+	public void setBanner_files_number(int banner_files_number) {
+		this.banner_files_number = banner_files_number;
+	}
 
 	public int getNewsNumber() {
 		return newsNumber;
@@ -33,6 +44,6 @@ public class AdminNewsBannerDTO {
 	@Override
 	public String toString() {
 		return "AdminNewsBannerDTO [newsNumber=" + newsNumber + ", newsTitle=" + newsTitle + ", newsCreatedDate="
-				+ newsCreatedDate + "]";
+				+ newsCreatedDate + ", banner_files_number=" + banner_files_number + "]";
 	}
 }
