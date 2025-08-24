79ea306db0e4dcfda76f689120412f36204a7ae9
jeabin5792 <woqls0512@naver.com>
2025-08-24 22:17:13 +0900

    250824 22:47 이재빈

--- workTogether/src/main/java/com/wt/app/careMyPage/CareMarkController.java
+++ workTogether/src/main/java/com/wt/app/careMyPage/CareMarkController.java
@@ -1,7 +1,25 @@
 package com.wt.app.careMyPage;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.wt.app.Execute;
+import com.wt.app.Result;
 
 public class CareMarkController implements Execute{
+
+	@Override
+	public Result Execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+Result result = new Result();
+		
+		
+		result.setPath("/app/myPageCare/careMark.jsp");
+	    result.setRedirect(true);
+	     return result;
+	}
 
 }
