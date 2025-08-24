fda7377c0df212130d8bcf543a2d3673a4fed0a1
jeabin5792 <woqls0512@naver.com>
2025-08-24 11:04:34 +0900

    250824 11:04 이재빈

--- workTogether/src/main/java/com/wt/app/careMyPage/CarePwCheckController.java
+++ workTogether/src/main/java/com/wt/app/careMyPage/CarePwCheckController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.wt.app.Execute;
 import com.wt.app.Result;
@@ -18,7 +19,13 @@ public class CarePwCheckController implements Execute{
 
 		UsersDAO usersDAO = new UsersDAO();
 		Result result = new Result();
+		HttpSession session = request.getSession();
+		
+		String usersPassword = request.getParameter("usersPassword");
+		Integer usersNumber = (Integer) session.getAttribute("usersNumber");
 		
 		
+		
+		return result;
 	}	
 }
