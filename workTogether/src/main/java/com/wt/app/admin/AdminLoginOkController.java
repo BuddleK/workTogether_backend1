df37f6213e28c84f06310233e4857564b1c73582
HwangSeungU <pett1475321@gmail.com>
2025-08-25 18:16:33 +0900

    250825 18:16 황승우 발표준비 커밋

--- workTogether/src/main/java/com/wt/app/admin/AdminLoginOkController.java
+++ workTogether/src/main/java/com/wt/app/admin/AdminLoginOkController.java
@@ -38,7 +38,7 @@ public class AdminLoginOkController implements Execute{
         
         if (adminNumber != -1) {
             // 로그인 성공 → 세션 저장 후 관리자 화면으로
-        	path = "/app/admin/userManager.ad";
+        	path = "/admin/care/list.ad";
             session.setAttribute("adminNumber", adminNumber);
             System.out.println("세션값 : " + adminNumber);
             
