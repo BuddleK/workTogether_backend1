42598aa7e823d073c1c0749116d9a58650430a0a
jeabin5792 <woqls0512@naver.com>
2025-08-25 09:51:46 +0900

    250825 09:51 이재빈

--- workTogether/src/main/java/com/wt/app/careMyPage/CareMarkOkController.java
+++ workTogether/src/main/java/com/wt/app/careMyPage/CareMarkOkController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.wt.app.Execute;
 import com.wt.app.Result;
@@ -18,6 +19,17 @@ public class CareMarkOkController implements Execute{
 		CareMarkDAO careMarkDAO = new CareMarkDAO();
 		Result result = new Result();
 
+		HttpSession session = request.getSession();
+		// 로그인 한 회원 정보 가져오기
+		Integer usersNumber = (Integer) request.getSession().getAttribute("usersNumber");
+		
+		String usersPfavoritesNumberassword = request.getParameter("favoritesNumber");
+		String favoritesShopsNumber = request.getParameter("favoritesShopsNumber");
+		String favoritesCreatedDate = request.getParameter("favoritesCreatedDate");
+		String shopsName = request.getParameter("shopsName");
+		String shopsRoadAddress = request.getParameter("shopsRoadAddress");
+		String shopsPhone = request.getParameter("shopsPhone");
+		
 		
 
 		result.setPath("/myPageCare/careMark.cp");
