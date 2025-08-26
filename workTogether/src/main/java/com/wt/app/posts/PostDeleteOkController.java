63448ee058ddeb4a2a4440316432c215caa7bf65
BuddleK <kangbudle01@gmail.com>
2025-08-27 04:27:27 +0900

    250827 04:27 게시물 삭제 완료

--- workTogether/src/main/java/com/wt/app/posts/PostDeleteOkController.java
+++ workTogether/src/main/java/com/wt/app/posts/PostDeleteOkController.java
@@ -8,21 +8,27 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.wt.app.Execute;
 import com.wt.app.Result;
+import com.wt.app.posts.dao.PostsDAO;
 
 public class PostDeleteOkController implements Execute{
 
 	@Override
 	public Result execute(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
+		PostsDAO postsDAO = new PostsDAO();
+//		
 		Result result = new Result();
         System.out.println("===============삭제오케이컨트롤러 진입================");
-        int postsNumber = Integer.valueOf(request.getParameter("postsNumber"));
+        int postsNumber = Integer.parseInt(request.getParameter("postsNumber"));
         
-        System.out.println("0의 valueOf는? " + Integer.valueOf(null));
+        //System.out.println("0의 valueOf는? " + Integer.valueOf(null));
+        
+        System.out.println("postsNumber는? " + postsNumber);
+        
+        postsDAO.delete(postsNumber);
         
         result.setPath("/post/postMain.po");
-		return null;
+		return result;
 	}
 	
 }
