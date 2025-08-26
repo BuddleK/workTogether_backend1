56ef05faf0e4aefa6b7377676037205786b35718
HwangSeungU <pett1475321@gmail.com>
2025-08-27 01:14:29 +0900

    250827 0114 황승우 쪽지 전송 가능

---
+++ workTogether/src/main/java/com/wt/app/dto/MessageSRDTO.java
@@ -0,0 +1,78 @@
+package com.wt.app.dto;
+
+public class MessageSRDTO {
+	private int messageNumber;
+	private int  careNumber;
+	private int normalNumber;
+	private String senderType;
+	private String messageContents;
+	private String messageDate;
+	private String isRead;
+	
+	private String usersNumber;
+	private String usersName;
+	public int getMessageNumber() {
+		return messageNumber;
+	}
+	public void setMessageNumber(int messageNumber) {
+		this.messageNumber = messageNumber;
+	}
+	public int getCareNumber() {
+		return careNumber;
+	}
+	public void setCareNumber(int careNumber) {
+		this.careNumber = careNumber;
+	}
+	public int getNormalNumber() {
+		return normalNumber;
+	}
+	public void setNormalNumber(int normalNumber) {
+		this.normalNumber = normalNumber;
+	}
+	public String getSenderType() {
+		return senderType;
+	}
+	public void setSenderType(String senderType) {
+		this.senderType = senderType;
+	}
+	public String getMessageContents() {
+		return messageContents;
+	}
+	public void setMessageContents(String messageContents) {
+		this.messageContents = messageContents;
+	}
+	public String getMessageDate() {
+		return messageDate;
+	}
+	public void setMessageDate(String messageDate) {
+		this.messageDate = messageDate;
+	}
+	public String getIsRead() {
+		return isRead;
+	}
+	public void setIsRead(String isRead) {
+		this.isRead = isRead;
+	}
+	public String getUsersNumber() {
+		return usersNumber;
+	}
+	public void setUsersNumber(String usersNumber) {
+		this.usersNumber = usersNumber;
+	}
+	public String getUsersName() {
+		return usersName;
+	}
+	public void setUsersName(String usersName) {
+		this.usersName = usersName;
+	}
+	@Override
+	public String toString() {
+		return "MessageSRDTO [messageNumber=" + messageNumber + ", careNumber=" + careNumber + ", normalNumber="
+				+ normalNumber + ", senderType=" + senderType + ", messageContents=" + messageContents
+				+ ", messageDate=" + messageDate + ", isRead=" + isRead + ", usersNumber=" + usersNumber
+				+ ", usersName=" + usersName + "]";
+	}
+	
+	
+	
+}
