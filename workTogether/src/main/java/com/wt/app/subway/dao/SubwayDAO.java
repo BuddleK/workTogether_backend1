d3451dfbe122224214354ca8b9381f931d18b502
dece1225 <dece1225@naver.com>
2025-09-01 15:10:25 +0900

    250901 15:10 김태현 작업 중 커밋

--- workTogether/src/main/java/com/wt/app/subway/dao/SubwayDAO.java
+++ workTogether/src/main/java/com/wt/app/subway/dao/SubwayDAO.java
@@ -12,25 +12,30 @@ import com.wt.config.MyBatisConfig;
 public class SubwayDAO {
 	public SqlSession sqlSession;
 
+	//DAO 기본 생성자 호출(sqlSession 동작 위한 작업)
 	public SubwayDAO() {
 		sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
 	}
 	
+	// 지하철 호선 조회 Mapper 불러오기 위한 메소드
 	public List<GetSubwayLineDTO> subwayLineSearch(){
 		List<GetSubwayLineDTO> list = sqlSession.selectList("subway.subwayLineSearch");
 		return list;
 	}
 
+	// 지하철 역 목록 이미지 조회 Mapper 불러오기 위한 메소드
 	public List<SubwayStationDTO> subwayStationImgSearch(){
 		List<SubwayStationDTO> img = sqlSession.selectList("subway.subwayStationImgSearch");
 		return img;
 	}
 	
+	// 지하철 역 이름 조회 Mapper 불러오기 위한 메소드
 	public List<SubwayStationDTO> subwayStationSearch(){
 		List<SubwayStationDTO> list = sqlSession.selectList("subway.subwayStationSearch");
 		return list;
 	}
 	
+	// 지하철 디테일 페이지 정보 조회 Mapper 불러오기 위한 메소드
 	public List<SubwayStationDTO> subwayDetailSearch(){
 		List<SubwayStationDTO> list = sqlSession.selectList("subway.subwayDetailSearch");
 		return list;
