146156d4cb696e03adfd1346e340a10eb45f0971
dece1225 <dece1225@naver.com>
2025-09-09 17:55:25 +0900

    250909 17:54 김태현 관리자 공지사항 작업 중

--- workTogether/src/main/webapp/assets/js/modal/modalDeleteMember.js
+++ workTogether/src/main/webapp/assets/js/modal/modalDeleteMember.js
@@ -1,10 +1,10 @@
 window.addEventListener('DOMContentLoaded', () => {
-  // html 문서에 모달 불러오기
-  fetch('${pageContext.request.contextPath}/app/modal/modalDeleteMember.jsp')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('modalDeleteMember').innerHTML = data;
-    });
+	// html 문서에 모달 불러오기
+	fetch(contextPath + '/app/modal/modalDeleteMember.jsp')
+		.then(response => response.text())
+		.then(data => {
+			document.getElementById('modalDeleteMember').innerHTML = data;
+		});
 });
 
 //모달 요소 가져오기
@@ -12,39 +12,36 @@ const modalDeleteMember = document.getElementById("modalDeleteMember");
 
 // 모달이 나타나는 함수(버튼에 추가하여 사용)
 function modalDeleteMemberShow() {
-   event.preventDefault();
-  console.log("모달버튼클릭");
-  modalDeleteMember.style.display = "flex";
+	event.preventDefault();
+	console.log("모달버튼클릭");
+	modalDeleteMember.style.display = "flex";
 
-  //엔터로 확인 버튼 누르기
-  document.onkeydown = (event) => {
-    if(event.key === "Enter"){
-      modalDeleteMemberCheck();
-    }
-  };
+	//엔터로 확인 버튼 누르기
+	document.onkeydown = (event) => {
+		if (event.key === "Enter") {
+			modalDeleteMemberCheck();
+		}
+	};
 }
 //모달 끄기 함수(모달 내부 x이미지에 추가되어 있음)
-function modalDeleteMemberNone(){
-  modalDeleteMember.style.display = "none";
-  document.onkeydown = null; // onkeydown 이벤트 제거
-}
-// 링크이동
-const link = "${pageContext.request.contextPath}/admin/adminUserManagerList.ad"
-function linkHref() {
-  location.href=link;
+function modalDeleteMemberNone() {
+	modalDeleteMember.style.display = "none";
+	document.onkeydown = null; // onkeydown 이벤트 제거
 }
 //확인 버튼
-function modalDeleteMemberCheck(){
-  console.log("회원 삭제 선택");
-  alert("회원 삭제 선택")
-  modalDeleteMemberNone();
-  linkHref();
+function modalDeleteMemberCheck() {
+	console.log("회원 삭제 선택");
+	alert("회원이 삭제되었습니다.");
+	modalDeleteMemberNone();
+
+	// 삭제 form submit
+	document.getElementById("deleteMemberForm").submit();
 }
 
 //취소 버튼
-function modalDeleteMemberCancel(){
-  console.log("회원 삭제 취소");
-  alert("회원 삭제 취소")
-  modalDeleteMemberNone();
+function modalDeleteMemberCancel() {
+	console.log("회원 삭제 취소");
+	alert("회원 삭제 취소")
+	modalDeleteMemberNone();
 }
 modalDeleteMemberNone();
