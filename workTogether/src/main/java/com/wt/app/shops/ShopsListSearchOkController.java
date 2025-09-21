363e94e81b9d90582a599ed3c102f306d6c51471
HwangSeungU <pett1475321@gmail.com>
2025-09-21 22:09:38 +0900

    250921 2209 황승우

--- workTogether/src/main/java/com/wt/app/shops/ShopsListSearchOkController.java
+++ workTogether/src/main/java/com/wt/app/shops/ShopsListSearchOkController.java
@@ -27,6 +27,19 @@ public class ShopsListSearchOkController implements Execute {
 		String shopsLegalDong = request.getParameter("legalDong");
 		shopsLegalDong = shopsLegalDong.replaceAll(" ", "");
 		System.out.println("동" + shopsLegalDong);
+		
+		List<ShopsListDTO> adminDongList = shopsDAO.adminDongSelect(); 
+		request.setAttribute("adminDongList", adminDongList);
+
+		// 선택된 지역 번호 계산
+		int leNum = 0;
+		for(int i = 0; i < adminDongList.size(); i++) {
+		    if(adminDongList.get(i).getShopsAdminDong().equals(shopsLegalDong)) {
+		        leNum = i;
+		        break;
+		    }
+		}
+		request.setAttribute("leNum", leNum);
 
 		String tmp = request.getParameter("page");
 		int page = (tmp == null) ? 1 : Integer.valueOf(tmp);
@@ -74,7 +87,6 @@ public class ShopsListSearchOkController implements Execute {
 		
 		
 		//법정 동명 전부 가져오기
-		List<ShopsListDTO> adminDongList = shopsDAO.adminDongSelect();
 		request.setAttribute("adminDongList", adminDongList);
 
 		result.setPath("/app/restaurant/restaurantDong.jsp");
